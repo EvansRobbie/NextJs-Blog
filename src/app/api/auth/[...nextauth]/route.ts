1575dc14b68fb1cd016094248b4ab03017a6268d
EvansRobbie <evansitoden94@gmail.com>
2023-07-03 01:37:53 +0300

    login with credentials and next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,50 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import connect from "@/utils/db";
+import User from "@/models/User";
+import bcrypt from 'bcryptjs';
+
+// const credentialsProvider = createCredentialsProvider();
+
 const handler = NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GoogleProvider({
-      clientId: process.env.Google_ID as string,
-      clientSecret: process.env.Google_SECRET as string,
-    }),
-    // ...add more providers here
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "email@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: Record<"email" | "password", string> | undefined) {
+        await connect();
+        try {
+          const user = await User.findOne({
+            email: credentials?.email
+          });
+          if (user) {
+            // check password
+            if (credentials?.password && user.password){
+
+              const passOk = await bcrypt.compare(credentials?.password, user.password);
+              if (passOk) {
+                return user;
+              } else {
+                throw new Error("Wrong credentials");
+              }
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (error:any) {
+          throw new Error(error);
+        }
+      }
+    })
   ],
-})
-export {handler as GET, handler as POST}
+  pages: {
+    error: "/dashboard/login",
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/app/dashboard/(auth)/login/page.tsx
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -1,13 +1,54 @@
-"use client"
-import { signIn } from 'next-auth/react'
-import React from 'react'
+"use client";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const Login = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [err, setErr] = useState<boolean>(false);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // console.log(email, password, username)
+    try {
+      signIn("credentials", {
+        email,
+        password,
+      });
+      router.push("/dashboard");
+    } catch (error) {
+      console.log(error);
+      setErr(true);
+    }
+  };
   return (
-    <div>
-      <button onClick={() => signIn("google")}>Login With Google</button>
+    <div className="flex w-full h-full items-center gap-5 flex-col justify-center ">
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button className="py-2 rounded-xl text-slate-950 active:scale-110  duration-300 ease-in shadow-2xl shadow-secondary hover:shadow-slate-400 px-4 bg-secondary max-w-full">
+          Login
+        </button>
+      </form>
+      {err && "something went wrong"}
+      <div>
+        <button onClick={() => signIn("google")}>Login With Google</button>
+      </div>
+      <Link href="/dashboard/login">Create a new Account</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
